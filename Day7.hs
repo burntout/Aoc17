eddf9e435852d818ed7ba230af3851ab0322d142
Kate Dawson <dawsonka@chesterfield.ac.uk>
2017-12-08 16:35:08 +0000

    something?

--- Day7.hs
+++ Day7.hs
@@ -6,7 +6,7 @@ data Node = Node { name :: [Char]
 
 bar = Node {name="bar", weight = 9, children = [foo]}
 foo = Node {name="foo", weight = 10, children = []}
-baz = Node {name="baz", weight = 19, children = []}
+baz = Node {name="baz", weight = 12, children = [bar]}
 
 getName :: Node -> [Char]
 getName Node {name=n, weight = w, children = c} = n
@@ -26,3 +26,20 @@ addChild node child = Node name weight newChildren
         newChildren = children ++ [child]
 
 
+hasChildren :: Node -> Bool
+hasChildren n
+    | getChildren n == [] = False
+    | otherwise  = True
+
+--isChild :: Node -> [Node] -> Bool
+--isChild n ns 
+--    where
+--        childNodes = map getChildren ns
+
+-- getRootTree :: [Nodes] -> Node
+getRootTree ns = root
+    where
+        branches = filter (hasChildren) ns
+        childNodes = concatMap getChildren branches
+        root = head $ filter (\n -> not $ elem n childNodes)  ns
+        
